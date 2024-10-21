568d97e24f078f2a8f41ac1a3388377578738a8d
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-21 19:40:27 +0800

    Update figma.manifest.ts

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,7 +1,7 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
-  name: "Figma-Plugin-React-Vite",
-  id: "1419651496336343854",
+  name: "AICollab4Coupling-Dev",
+  id: "1430085371827286872",
   api: "1.0.0",
   main: "plugin.js",
   ui: "index.html",
