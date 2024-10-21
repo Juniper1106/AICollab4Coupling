61eec39cb2566b1b365bc7532f8d6c9465629da0
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-21 19:41:52 +0800

    modify sliders
    1. set range
    2. add marks

--- src/ui/components/SliderArea.tsx
+++ src/ui/components/SliderArea.tsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import "@ui/components/SliderArea.scss"
-import { Slider } from "antd";
+import { Slider, Typography } from "antd";
+import { InfoCircleTwoTone, InfoCircleOutlined } from "@ant-design/icons";
+import { SliderSingleProps } from "antd";
+
+const { Text } = Typography;
+
+const marks: SliderSingleProps['marks'] = {
+    1: '小',
+    2: '中',
+    3: '大',
+}
 
 function SliderArea() {
     const [disabled, setDisabled] = useState(false);
 
     return (
     <div className="sliderContainer">
-        <Slider defaultValue={30} disabled={disabled} />
-        <Slider defaultValue={30} disabled={disabled} />
+        <Text>AI视野范围 <InfoCircleOutlined style={{color: '#444444'}} /></Text>
+        <Slider defaultValue={2} max={3} min={1} marks={marks} disabled={disabled} />
+
+        <Text>AI记忆范围 <InfoCircleOutlined style={{color: '#444444'}} /></Text>
+        <Slider defaultValue={2} max={3} min={1} marks={marks} disabled={disabled} />
     </div>
     );
 }
