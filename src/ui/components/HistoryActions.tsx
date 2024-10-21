bd393b70553623d6adbd52c31e0f83dc64a37e79
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-21 19:42:44 +0800

    modify layout

--- src/ui/components/HistoryActions.tsx
+++ src/ui/components/HistoryActions.tsx
@@ -1,4 +1,4 @@
-import { Avatar, List } from 'antd';
+import { List, Typography } from 'antd';
 import "@ui/components/HistoryActions.scss"
 
 const data = [
@@ -8,29 +8,32 @@ const data = [
   {
     title: 'Ant Design Title 2',
   },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
+  // {
+  //   title: 'Ant Design Title 3',
+  // },
+  // {
+  //   title: 'Ant Design Title 4',
+  // },
 ];
+
+const { Title } = Typography;
 
 function HistoryActions() {
     return (
         <div className='historyActions'>
-            <List
-              itemLayout="horizontal"
-              dataSource={data}
-              renderItem={(item, index) => (
-                <List.Item>
-                  <List.Item.Meta
-                    title={<a href="https://ant.design">{item.title}</a>}
-                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                  />
-                </List.Item>
-              )}
-            />
+          <Title level={5}>AI行为历史</Title>
+          <List
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={(item, index) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={<a href="https://ant.design">{item.title}</a>}
+                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+              </List.Item>
+            )}
+          />
         </div>
     )
 }
