9a20e9c9d433b49b4124924a918e3bb729d62c1f
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-22 13:00:29 +0800

    optimize layout

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -2,17 +2,14 @@ import "@ui/styles/main.scss";
 
 import AIAvatar from "./components/AIAvatar";
 import SliderArea from "./components/SliderArea";
-import HistoryActions from "./components/HistoryActions";
-import ChatHistory from "./components/ChatHistory"
-import InputBox from "./components/InputBox";
+import HistoryArea from "./components/HistoryArea";
 
 function App() {
   return (
     <div className="homepage">
       <AIAvatar />
       <SliderArea />
-      <HistoryActions />
-      <InputBox />
+      <HistoryArea />
     </div>
   );
 }
