cd4350c77da09ae5a80df79b64178c13074575a9
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-29 21:04:20 +0800

    add login page

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,13 +1,45 @@
+import React from 'react';
+import { useState } from "react";
+import { UserOutlined } from '@ant-design/icons';
+import { Input, Button } from 'antd';
 import "@ui/styles/main.scss";
-
+import "@ui/components/LoginPageForProbe.scss"
 import SliderArea from "./components/SliderArea";
 import HistoryArea from "./components/HistoryArea";
 
+
 function App() {
+  const [login, setLogin] = useState(false);
+  
+  const LoginPageForProbe: React.FC = () => (
+    <div className="loginPageForProbe">
+      <Input size="large" placeholder="请输入被试编号" prefix={<UserOutlined />} />
+      <br />
+      <br />
+      <Button type="primary" onClick={handleLogin}>提交</Button>
+    </div>
+  );
+
+  function handleLogin() {
+    if (login === false) {
+      setLogin(true)
+    }
+  }
+
+  function switchPage() {
+    if (login === false)
+      return <LoginPageForProbe />
+    else return (
+      <>
+        <SliderArea />
+        <HistoryArea />
+      </>
+    )
+  }
+
   return (
-    <div>
-      <SliderArea />
-      <HistoryArea />
+    <div className="homepage">
+      {switchPage()}
     </div>
   );
 }
