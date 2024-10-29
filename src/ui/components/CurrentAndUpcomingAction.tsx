5bf6dbe3ae63ce360e4ce5c7c06e011b5c10ae05
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-10-29 13:11:54 +0800

    update current actions area dot style

--- src/ui/components/CurrentAndUpcomingAction.tsx
+++ src/ui/components/CurrentAndUpcomingAction.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Timeline } from 'antd';
+import { SyncOutlined } from '@ant-design/icons';
 import '@ui/components/CurrentAndUpcomingAction.scss';
 
 const App: React.FC = () => (
@@ -8,9 +9,11 @@ const App: React.FC = () => (
           items={[
             {
               children: 'Create a services site 2015-09-01',
+              dot: <SyncOutlined spin />,
             },
             {
               children: 'Solve initial network problems 2015-09-01',
+              color: 'gray',
             },
           ]}
         />
