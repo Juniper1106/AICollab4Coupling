bab0953dedb488e32d6b52acc054d31285bbb336
王熙来 <3210104144@zju.edu.cn>
2024-11-11 14:17:22 +0800

    debug screenshot

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -3,16 +3,9 @@ import { initializeNetwork } from "@common/network/init";
 import { NetworkSide } from "@common/network/sides";
 import { NetworkMessages } from "@common/network/messages";
 
-function debounce(func: (...args: any[]) => any, delay: number) {
-	let timeoutId: number;
-	return (...args: any[]) => {
-		if (timeoutId) clearTimeout(timeoutId);
-		timeoutId = setTimeout(() => func(...args), delay);
-	};
-}
-
 let lastSelection: readonly SceneNode[] = [];
-const debouncedCommitOperation = debounce(commitOperation, 500);
+let messages: Set<string> = new Set();
+let propChanges: Map<string, Set<string>> = new Map();
 
 async function getCanvasScreenshot() {
 	const image = await figma.currentPage.exportAsync({
@@ -27,18 +20,11 @@ async function getCanvasScreenshot() {
 }
 
 async function getSelectionScreenshot() {
-	let selection : readonly SceneNode[] = figma.currentPage.selection;
-	if (selection.length === 0) {
+	if (lastSelection.length === 0) {
 		console.log('no selection to capture');
-		if (lastSelection.length === 0) {
-			return null;
-		} else if (figma.currentPage.children.includes(lastSelection[0])) {
-			selection = lastSelection;
-		} else {
-			return null;
-		}
+		return null;
 	}
-	const image = await selection[0].exportAsync({
+	const image = await lastSelection[0].exportAsync({
 		format: 'PNG', // 可选择 PNG 或 JPG
 		constraint: {
 			type: 'HEIGHT',
@@ -47,36 +33,6 @@ async function getSelectionScreenshot() {
 	});
 	const base64Image = figma.base64Encode(image);
 	return base64Image;
-}
-
-async function commitOperation(message: string, selection: readonly SceneNode[] = figma.currentPage.selection) {
-	let canvasScreenshot = await getCanvasScreenshot();
-	let selectionScreenshot = await getSelectionScreenshot();
-	if (selectionScreenshot === null) {
-		selectionScreenshot = canvasScreenshot;
-	}
-	const payload = {
-		message: message,
-		canvasScreenshot: canvasScreenshot,
-		selectionScreenshot: selectionScreenshot,
-		timeStamp: new Date().getTime()
-	};
-	await fetch(
-	  'http://127.0.0.1:5010/save_operation',
-	  {
-		method: 'POST',
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(payload)
-	  }
-	).then(
-	  response => response.text()
-	).then(
-	  text => console.log(text)
-	).catch(
-	  error => console.error(error)
-	)
 }
 
 async function bootstrap() {
@@ -107,15 +63,62 @@ async function bootstrap() {
 	figma.on('currentpagechange', async () => {
 		const message = `用户切换至页面: ${figma.currentPage.name}`
 		console.log(message);
-		await commitOperation(message);
+		messages.add(message);
+		lastSelection = [];
 	});
 
 	//选择改变
 	figma.on('selectionchange', async () => {
 		console.log('selction', figma.currentPage.selection);
-		if (figma.currentPage.selection.length != 0) {
+		console.log('lastSelection', lastSelection);
+		//如果lastSelection和当前selection的id一样,则直接返回
+		if (lastSelection.length === figma.currentPage.selection.length && lastSelection.every((node, index) => node.id === figma.currentPage.selection[index].id)) {
 			lastSelection = figma.currentPage.selection;
+			return;
 		}
+		if (messages.size === 0 && propChanges.size === 0) {
+			lastSelection = figma.currentPage.selection;
+			return;
+		}
+
+		let canvasScreenshot = await getCanvasScreenshot();
+		let selectionScreenshot = await getSelectionScreenshot();
+		if (selectionScreenshot === null) {
+			selectionScreenshot = canvasScreenshot;
+		}
+		if (propChanges.size > 0) {
+			for (const [message, props] of propChanges) {
+				messages.add(message+Array.from(props).join(', '));
+			}
+		}
+		const msg = Array.from(messages).join('; ');
+		console.log('msg:', msg);
+		const payload = {
+			message: msg,
+			canvasScreenshot: canvasScreenshot,
+			selectionScreenshot: selectionScreenshot,
+			timeStamp: new Date().getTime()
+		};
+		await fetch(
+		  'http://127.0.0.1:5010/save_operation',
+		  {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		  }
+		).then(
+		  response => response.text()
+		).then(
+		  text => console.log(text)
+		).catch(
+		  error => console.error(error)
+		)
+
+		lastSelection = figma.currentPage.selection;
+		messages.clear();
+		propChanges.clear();
 	});
 
 	//文档改变
@@ -127,21 +130,27 @@ async function bootstrap() {
 				switch (change.type) {
 					case "CREATE":
 						message = `用户创建${change.node.type}节点${change.node.id}`
+						// message = `user create ${change.node.type} node ${change.node.id}`
 						console.log(message);
-						await commitOperation(message);
+						messages.add(message);
 						break;
 
 					case "DELETE":
 						message = `用户删除${change.node.type}节点${change.node.id}`
+						// message = `user delete ${change.node.type} node ${change.node.id}`
 						console.log(message);
-						await commitOperation(message);
+						messages.add(message);
 						break;
 
 					case "PROPERTY_CHANGE":
-						const props = change.properties.join(', ');
-						message = `用户改变${change.node.type}节点${change.node.id}的属性: ${props}`
+						message = `用户改变${change.node.type}节点${change.node.id}的属性: `
+						// message = `user change ${change.node.type} node ${change.node.id} properties: `
+						if (propChanges.has(message)) {
+							change.properties.forEach(prop => propChanges.get(message)?.add(prop));
+						} else {
+							propChanges.set(message, new Set(change.properties));
+						}
 						console.log(message);
-						debouncedCommitOperation(message, figma.currentPage.selection);
 						break;
 				}
 			}
